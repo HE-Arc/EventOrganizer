03781c2622891364133cd40271d3d9df03c1a7e6
cyril.ruedin <cyril.ruedin@he-arc.ch>
2016-11-08 14:18:16 +0100

    my events page

--- resources/views/event/usereventlist.blade.php
+++ resources/views/event/usereventlist.blade.php
@@ -8,10 +8,21 @@
 
     <div id="main-container">
 
-        <div id="items" class="row col s12">
+        <div class="row col s6">
+            <div class="card horizontal">
+                <div class="card-stacked">
+                    <div class="card-content">
+                        <span class="card-title">{{$user->name}}t</span>
+                        <p>My events</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div id="events" class="collection">
 
             @forelse ($user->events()->get() as $event)
-                {{$event->description}}
+                <a href="event/{{ $event->id }}" class="collection-item"><span class="badge"> {{ $event->location }} at {{ $event->date }}</span>{{ $event->name }}</a>
             @empty
                 <p>Nothing to do, let's organize some event</p>
             @endforelse
