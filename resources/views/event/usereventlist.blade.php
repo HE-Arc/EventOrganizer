f6b6c6ec23fddbdf670d20114e2ae769240a2016
Cyril Ruedin <Cyril Ruedin>
2016-11-19 19:38:50 +0100

    Readme commit ?

--- resources/views/event/usereventlist.blade.php
+++ resources/views/event/usereventlist.blade.php
@@ -8,10 +8,33 @@
 
     <div id="main-container">
 
-        <div id="items" class="row col s12">
+
+        <div id="main-container">
+
+            <div class="row col s6">
+                <div class="card horizontal">
+                    <div class="card-image">
+                        @if ($user->image_url != null)
+                            <img alt="{{$event->name}} profile picture" src="{{$event->image_url}}">
+                        @else
+                            <img alt="" src="{{url('/')}}/imgs/event_picture_not_found.png">
+                        @endif
+                    </div>
+                    <div class="card-stacked">
+                        <div class="card-content">
+                            <span class="card-title">{{$event->name}}t</span>
+                            <p>{{$event->description}}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        <div id="events" class="collection">
+
+
 
             @forelse ($user->events()->get() as $event)
-                {{$event->description}}
+                <a href="event/{{$event->id}}" class="collection-item"><span class="badge">1</span>{{$event->description}}</a>
             @empty
                 <p>Nothing to do, let's organize some event</p>
             @endforelse
